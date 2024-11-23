bc498fe18daef1abbdad5500b4426c5613315dc0
NEELA-A <220701184@rajalakshmi.edu.in>
2024-11-23 23:06:25 +0530

    Update client.py

--- EX-12 End –End Communication at Transport Layer/Exp-12(b)/client.py
+++ EX-12 End –End Communication at Transport Layer/Exp-12(b)/client.py
@@ -1,15 +1,16 @@
 import socket
-def recvr1():
-    port=12345
+import time
+def recvr2(a):
     host='127.0.0.1'
+    port=12345
     with socket.socket(socket.AF_INET,socket.SOCK_DGRAM) as s:
-        s.bind((host,port))
-        while(True):
-            d,add=s.recvfrom(1024)
-            print("Client",{d.decode()})
-            a=input("Enter Reply ")
-            s.sendto(a.encode(),add)
-            if(a=="end"):
-                break
-                exit
-recvr1()        
+        s.sendto(a.encode(),(host,port))
+        d,addr=s.recvfrom(1024)
+        print({d.decode()})
+while(True):
+    a=input("Enter Message ")
+    if (a=="end"):
+        recvr2(a)
+        break
+    else:
+        recvr2(a)
